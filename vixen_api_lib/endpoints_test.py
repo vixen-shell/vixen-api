2e11b48b6786b4593c43b81039bd03d839cdd1ae
Nohavye <noha.poncelet@gmail.com>
2023-12-27 15:38:47 +0100

    Websockets Test.

--- vixen_api_lib/endpoints_test.py
+++ vixen_api_lib/endpoints_test.py
@@ -1,5 +1,25 @@
+import asyncio
+from fastapi import WebSocket
 from .api import api
 
 @api.get("/hello/{name}")
 async def hello(name: str):
     return {"hello": name}
+
+# WebSocket Send events messages
+@api.websocket("/websocket_events")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    i = 0
+    while True:
+        i += 1
+        await websocket.send_text(f"Message {i}")
+        await asyncio.sleep(1)
+
+# WebSocket Send and receive messages
+@api.websocket("/websocket_send_receive")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message textuel reçu : {data}")

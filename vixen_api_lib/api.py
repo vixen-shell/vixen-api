c2b015d144376c86f309b5173320ca5a542ceb39
Nohavye <noha.poncelet@gmail.com>
2024-01-31 17:06:16 +0100

    Preadd static files endpoints.

--- vixen_api_lib/api.py
+++ vixen_api_lib/api.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 api = FastAPI()
 
+# origins = [
+#     "http://localhost:4173"
+# ]
+
+# api.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 from . import hypr_endpoints
+# from . import static_endpoints
 
 HOST = '127.0.0.1'
 PORT = 8420
